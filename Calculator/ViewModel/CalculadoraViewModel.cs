f94481f97c8bf7f8774e284f6d5a8078e0e3c532
Pamela Loyola <kogan.03.10@gmail.com>
2019-10-28 15:30:43 -0500

    Lab11

---
+++ Calculator/ViewModel/CalculadoraViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Calculator.ViewModel
+{
+    public class CalculadoraViewModel : ViewModelBase
+    {
+
+        #region Propiedades
+        int valor1;
+
+        public int Valor1
+        {
+            get { return valor1; }
+            set
+            {
+                if (valor1 != value)
+                {
+                    valor1 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int valor2;
+        public int Valor2
+        {
+            get { return valor2; }
+            set
+            {
+                if (valor2 != value)
+                {
+                    valor2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int suma;
+        public int Suma
+        {
+            get { return suma; }
+            set
+            {
+                if (suma != value)
+                {
+                    suma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resta;
+        public int Resta
+        {
+            get { return resta; }
+            set
+            {
+                if (resta != value)
+                {
+                    resta = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region Comandos 
+        public ICommand Sumar { protected set; get; }
+
+        public ICommand Restar { protected set; get; }
+
+        public ICommand Multiplicar { protected set; get; }
+
+        public ICommand Dividir { protected set; get; }
+
+        #endregion
+
+        #region Constructor
+
+        public CalculadoraViewModel()
+        {
+
+            Sumar = new Command(() =>
+            {
+                Suma = Valor1 + Valor2;
+            });
+
+            Restar = new Command(() =>
+            {
+                Resta = Valor1 - Valor2;
+            });
+
+            Multiplicar = new Command(() =>
+            {
+                Producto = Valor1 * Valor2;
+            });
+
+            Dividir = new Command(() =>
+            {
+                Division = Valor1 / Valor2;
+            });
+
+        }
+        #endregion
+    }
+
+}
